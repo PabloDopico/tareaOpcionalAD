caf05fc953481ee77880feb4a48c9e96f512a021
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2023-02-14 13:40:04 +0100

    Completado paso 4 (leidos los errores) del tutorial de python

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,6 @@
 lado=int(input("Ingrese la medida del lado del cuadrado:"))
 superficie=lado*lado
 print("La superficie del cuadrado es")
+## print (Superficie) ---> error sintáctico
+## superficie = lado*lado*lado ---> error lógico 
 print(superficie)
