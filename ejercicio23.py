ebd4f71822bdf3562d55a3d4d55223c2861e0646
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2023-02-14 20:46:08 +0100

    Completado paso 8 del tutorial

---
+++ ejercicio23.py
@@ -0,0 +1,5 @@
+num1=int(input("Ingrese primer valor:"))
+num2=int(input("Ingrese segundo valor:"))
+num3=int(input("Ingrese tercer valor:"))
+if num1<10 or num2<10 or num3<10
+    print("Al menos un de los valorse ingresados es menor a 10")
---
+++ ejercicio24.py
@@ -0,0 +1,10 @@
+num1=int(input("Ingrese primer valor:"))
+num2=int(input("Ingrese segundo valor:"))
+num3=int(input("Ingrese tercer valor:"))
+if num1==num2 and num1==num3:
+    suma=num1+num2
+    print("La suma es")
+    print(suma)
+    producto=suma*num3
+    print("La suma del primero y segundo valor multiplicado por el tercero es")
+    print(producto)
