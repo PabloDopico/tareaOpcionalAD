ad26217710ae68d43ff5e9b5fb2fe95026663ff8
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2023-02-14 20:18:29 +0100

    Completado paso 6 del tutorial

---
+++ ejercicio11.py
@@ -0,0 +1,15 @@
+num1=int(input("Ingrese primer valor:"))
+num2=int(input("Ingrese segundo valor:"))
+if num1>num2:
+    suma=num1+num2
+    diferencia=num1-num2
+    print ("La suma de los dos valores es")
+    print (suma)
+    print ("La diferencia es")
+    print (diferencia)
+else:
+    producto=num1*num2
+    division=num1/num2
+    print ("El producto es")
+    print (producto)
+    print (division)
---
+++ ejercicio13.py
@@ -0,0 +1,5 @@
+valor=int(input("Ingrese un valor comprendido entre 1 y 99:"))
+if valor>= 10:
+    print ("Tiene 2 dígitos")
+else:
+    print ("Tiene 1 digito")
